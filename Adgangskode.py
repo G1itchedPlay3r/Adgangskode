7b17e885ab82a6b32493064281c7b718059c36b6
G1itchedPlay3r <100191431+G1itchedPlay3r@users.noreply.github.com>
2022-03-10 19:59:44 +0100

    til txt fil

--- Adgangskode.py
+++ Adgangskode.py
@@ -1,13 +1,27 @@
 import random
 from turtle import up
 
+
+#Adgangskode
 lower = "abcdefghijklmnopqrstuvwxyz"
 upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 numbers = "1234567890"
 symbols = "!?"
 
 all = lower + upper + numbers + symbols
-length = 16
+length = 10
 password = "".join(random.sample(all, length))
-print (password)
+
+#list to string
+def listToString(s): 
+    
+    str1 = ""
+    
+    return (str1.join(s))
+           
+s = ["new kode: ", password]
+
+#print to txt
+with open ("koder.txt", "w") as testtxt:
+    testtxt.write(listToString(s))
 
